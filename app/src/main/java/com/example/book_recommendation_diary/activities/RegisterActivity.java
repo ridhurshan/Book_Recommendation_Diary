f44ee95a88458650faeb8697eab1804aa1f7af47
ridhurshan <ridhurshan.ict@gmail.com>
2026-02-16 22:06:57 +0530

    Added Register page UI

--- app/src/main/java/com/example/book_recommendation_diary/activities/RegisterActivity.java
+++ app/src/main/java/com/example/book_recommendation_diary/activities/RegisterActivity.java
@@ -1,4 +1,34 @@
 package com.example.book_recommendation_diary.activities;
 
-public class RegisterActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.book_recommendation_diary.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText etUsername, etEmail, etPassword;
+    Button btnRegister;
+    TextView tvLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etUsername = findViewById(R.id.etUsername);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+        tvLogin = findViewById(R.id.tvLogin);
+
+        btnRegister.setOnClickListener(v -> {
+            Toast.makeText(this, "Register button clicked", Toast.LENGTH_SHORT).show();
+        });
+    }
 }
